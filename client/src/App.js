81dc8c4a3d49df91bd59a13d72a3e7536ec8c67b
Shahin Imtiaz <shahin.imtiaz@mail.utoronto.ca>
2021-01-16 20:03:55 -0500

    Misc. fixes

--- client/src/App.js
+++ client/src/App.js
@@ -85,15 +85,6 @@ class NewGallery extends Component {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="mr-auto">
-            {/* <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link> */}
-            {/* <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-            </NavDropdown> */}
             <button class="btn btn-primary" type="submit" onClick={this.toggleUploadModal}>
               Upload
             </button>
